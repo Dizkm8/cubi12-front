bcacfd7c4e2818bb48133cc84bb9732154facccb
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-01 18:01:31 -0300

    feat: base packages added

--- cubitwelve/src/App.tsx
+++ cubitwelve/src/App.tsx
@@ -1,5 +1,7 @@
+import { Typography } from "@mui/material";
+
 function App() {
-  return <div>Hello World!</div>;
+  return <Typography variant="h1">Hello World!</Typography>;
 }
 
 export default App;
