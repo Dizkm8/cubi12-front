157e20d54d8ae171556d272d0709c0f147e07e1b
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-11-04 14:24:30 -0300

    CUB-198

--- cubitwelve/src/app/layout/App.tsx
+++ cubitwelve/src/app/layout/App.tsx
@@ -1,7 +1,16 @@
+import { Container } from "@mui/material";
+import HomePage from "../../features/home/HomePage";
 import Navbar from "../components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Container maxWidth="xl" sx={{ paddingTop: "3rem" }}>
+        <HomePage />
+      </Container>
+    </>
+  );
 }
 
 export default App;
