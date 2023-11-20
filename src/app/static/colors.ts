3b8a1d89206fe06a050707559e86f7d865e141f4
GioxBRUSH <105091966+GioxBRUSH@users.noreply.github.com>
2023-11-20 19:43:13 -0300

    change repo

---
+++ src/app/static/colors.ts
@@ -0,0 +1,10 @@
+export const primaryBlueColor = "#1C478F";
+
+ 
+export const primaryOrangeColor = "#F7B52C"; 
+
+
+export const primaryGrayColor = "#C3C3C3 "; 
+
+
+export const primaryRedColor = "#D90000";
