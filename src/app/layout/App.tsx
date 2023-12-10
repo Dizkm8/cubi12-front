93803eca3b46d0a291d7980a4a3acb4fa9874536
GioxBRUSH <105091966+GioxBRUSH@users.noreply.github.com>
2023-12-10 00:31:34 -0300

    feat: add token expire verification

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
 import Routes from "../router/Routes";
 import { SubjectCodeProvider } from "../context/SubjectCodeContext";
+import {jwtDecode} from 'jwt-decode';
+import { AuthContext } from "../../app/context/AuthContext";
 
 function App() {
-  // Obtener el token y revisar si expiró
+  const token = localStorage.getItem('token');
+  const { setAuthenticated } = useContext(AuthContext);
 
-  // Si ya expiró, lo borras y seteas el contexto en no logggeado
-
-  // Si no ha expirado, no haces nada
-  // LLamar a funcion
+  if (token) {
+    const decodedToken: any = jwtDecode(token);
+    const currentDate = new Date();
+    if (decodedToken.exp * 1000 < currentDate.getTime()) {
+      localStorage.removeItem('token');
+      setAuthenticated(false);
+      console.log('Token expirado');
+  }
+}
 
   return (
     <>
