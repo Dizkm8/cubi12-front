c888b741333a10b6e3230aedc79037b600950703
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-12-10 01:10:47 -0300

    fix: regex now support spanish letters

--- src/app/utils/Regex.ts
+++ src/app/utils/Regex.ts
@@ -2,8 +2,8 @@ const emailRegex =
   /^[a-zA-Z]+(?:\.[a-zA-Z]+)?\d*?@(?:([a-zA-Z]+\.)+)?\ucn\.cl$/;
 const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{10,16}$/;
 const rutRegex = /^(\d{1,3}(\.\d{3})*-\d|(\d{1,3}(\.\d{3})*-[Kk]))$/;
-const nameRegex = /^[a-zA-Z]{3,50}$/;
-const lastNameRegex = /^[a-zA-Z]{3,30}$/;
+const nameRegex = /^[a-zA-ZñáéíóúüÜÁÉÍÓÚÑ\s]{3,50}$/;
+const lastNameRegex = /^[a-zA-ZñáéíóúüÜÁÉÍÓÚÑ\s]{3,30}$/;
 
 const Regex = {
   emailRegex,
