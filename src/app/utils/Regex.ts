13f5f044ef9c2a03040a4149cb77b97ab7596886
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-12-11 14:54:16 -0300

    fix: email regex

--- src/app/utils/Regex.ts
+++ src/app/utils/Regex.ts
@@ -5,7 +5,7 @@ const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{10,16}$/;
 const rutRegex = /^(\d{1,3}(\.\d{3})*-\d|(\d{1,3}(\.\d{3})*-[Kk]))$/;
 const nameRegex = /^[a-zA-ZñáéíóúüÜÁÉÍÓÚÑ\s]{3,50}$/;
 const lastNameRegex = /^[a-zA-ZñáéíóúüÜÁÉÍÓÚÑ\s]{3,30}$/;
-
+  
 const Regex = {
   emailRegex,
   pwdRegex,
