08f6bd0facd3c2aaf9ca7d8421e362bb223ee32d
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2023-12-11 14:46:27 -0300

    fix: insensitive case for emails

--- src/app/utils/Regex.ts
+++ src/app/utils/Regex.ts
@@ -1,5 +1,6 @@
 const emailRegex =
-  /^[a-zA-Z]+(?:\.[a-zA-Z]+)?\d*?@(?:([a-zA-Z]+\.)+)?\ucn\.cl$/;
+  /^[a-zA-Z]+(?:\.[a-zA-Z]+)?\d*?@(?:([a-zA-Z]+\.)+)?ucn\.cl$/i;
+
 const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{10,16}$/;
 const rutRegex = /^(\d{1,3}(\.\d{3})*-\d|(\d{1,3}(\.\d{3})*-[Kk]))$/;
 const nameRegex = /^[a-zA-ZñáéíóúüÜÁÉÍÓÚÑ\s]{3,50}$/;
